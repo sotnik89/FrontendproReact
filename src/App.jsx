33e2c7a310b57770d03034169ffa36b583f4345c
sotnik89 <sotnik89@gmail.com>
2026-05-04 17:10:43 +0300

    changed_App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import React from "react";
+import Header from "./components/Header/Header.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+import MyApp from "./pages/MyApp/MyApp.jsx";
 
+class App extends React.Component{
+  render() {
+    return(
+        <div>
+          <Header></Header>
+          <MyApp></MyApp>
+          <Footer></Footer>
 
-function App() {
-  return(
-      <div></div>
-  )
+        </div>
+    )
+  }
 }
 
 export default App
